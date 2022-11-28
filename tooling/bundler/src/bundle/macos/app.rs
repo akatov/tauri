66566076a928af6cb296d20fc5c2cc099d7349b0
Dmitri Akatov <dmitri@akatov.com>
2022-11-28 23:11:34 +0000

    update dylib load paths for dylibs as well

--- tooling/bundler/src/bundle/macos/app.rs
+++ tooling/bundler/src/bundle/macos/app.rs
@@ -267,7 +267,29 @@ fn copy_frameworks_to_bundle(bundle_directory: &Path, settings: &Settings) -> cr
         .file_name()
         .expect("Couldn't get framework filename");
       if src_path.exists() {
-        common::copy_file(&src_path, &dest_dir.join(&src_name))?;
+        let dest_path = &dest_dir.join(&src_name);
+        common::copy_file(&src_path, &dest_path)?;
+        for framework2 in frameworks.iter() {
+          if !framework.ends_with(".dylib") {
+            continue;
+          }
+          let lib_path = PathBuf::from(framework2);
+          if lib_path.exists() {
+            let lib_name = lib_path
+              .file_name()
+              .expect("Couldn't get lib filename")
+              .to_str()
+              .expect("");
+            info!(action = "Install_name_tool"; "for {} with {}", &dest_path.display(), &lib_name);
+            Command::new("install_name_tool")
+              .arg("-change")
+              .arg(&lib_path)
+              .arg(format!("@executable_path/../Frameworks/{}", &lib_name))
+              .arg(&dest_path)
+              .output_ok()
+              .context("failed to correct lib")?;
+          }
+        }
       }
       continue;
     } else if framework.contains('/') {
